3aeb9b883920e47cd9f152cc7962cd2be095fa9b
Romakin Sergey <romakinsv@mail.ru>
2013-08-21 13:21:46 +0400

    Initial Commit

---
+++ Clock_test2/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Clock_test2
+//
+//  Created by Sergey Romakin on 21.08.13.
+//  Copyright (c) 2013 Sergey Romakin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Clock_test2/FirstViewController.h
@@ -0,0 +1,13 @@
+//
+//  FirstViewController.h
+//  Clock_test2
+//
+//  Created by Sergey Romakin on 21.08.13.
+//  Copyright (c) 2013 Sergey Romakin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FirstViewController : UIViewController
+
+@end
